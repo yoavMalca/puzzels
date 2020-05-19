ea435357f91c34067008c2dc2451b88811324e21
yoavMalca <55780551+yoavMalca@users.noreply.github.com>
2020-05-19 19:57:15 +0300

    Add files via upload

--- Client.py
+++ Client.py
@@ -17,21 +17,26 @@ yyy1=-1
 class new_but:
     def __init__(self,image1,root,g,b,okVar,xxx,yyy):
         self.Butt=tk.Button(root, image = image1,command=lambda:self.ret_tup(okVar,xxx,yyy))
-        self.image=image1
-        self.tup=(g,b)
-        self.okVar=okVar
+        self.image=image1#התמונה הנמצאתבכפתור 
+        self.tup=(g,b)#מיקום כל כפתור 
+        self.okVar=okVar# משתנה המודיע על לחיצת הכפתור 
+    # משתנה המחזיר את הכפתור 
     def ret_but(self):
         return self.Butt
+    #מחזיר את תהמונה הכפתור 
     def ret_img(self):
         return self.image
+    # מחזיר את מיקום הכפתור 
     def ret_tup(self,okVar,xxx,yyy):
         okVar.set(1)
         print (self.tup)
-        global xxx1
-        global yyy1
+        global xxx1#מיקום הכתור הלחוץ כרגע 
+       
+        global yyy1#מיקום הכפתור הלחוץ כרגע 
         xxx1=self.tup[0]
         yyy1=self.tup[1]
         return self.tup
+# פעולת קבלת הסדר של המחסן בתחילת המשחק 
 def start_game(string):
     order=[[]]
     arr=string.split("|||")
@@ -54,27 +59,28 @@ def start_game(string):
 
 class client:
     def __init__(self,name,imageObject,order,server_socket):
-        self.name=name
-        self.items=[[]]
-        self.imageObject=imageObject
-        self.all_image=self.crop_Array(imageObject)
-        self.items=[[]]
+        self.name=name#שם המשתמש 
+        self.items=[[]]#מערך דו מימדי המכיל את כל החלקים 
+        self.imageObject=imageObject#שם תמונת הפאזל
+        self.all_image=self.crop_Array(imageObject)# התמונה הראשונית מחלוקת לחלקים 
+        self.items=[[]]#מחסן המשתמשים 
         self.server_socket=server_socket
-        self.root=tk.Tk()
+        self.root=tk.Tk()#חלון הגרפיקה
         self.lastdata=""
-        self.forgotten=[]
-        self.anotherone=[]
+        self.forgotten=[]# רשימת החלקים שבהם עשינו שימוש במהלך המשחק
+        self.anotherone=[]# רשימת  החלקים המופיע על לוח הפאזל ואיזה חלקים אלו 
         for i in range(len(order)):
             for t in range (len(order[i])):
                 x1,y1=order[i][t]
                 self.items[i].append((self.all_image[x1][y1],(x1,y1)))
             self.items.append([])
+    #פעולה הלוקחת תמונה וחותכת אותה לחלק    
     def image_crop(self,x1,y1,imageObject): 	
         cropped = imageObject.crop((x1,y1,x1+(imageObject.size[0]/3),y1+imageObject.size[1]/3))
         return cropped
-    
+    #פעולה המפענחת את המיגע המתקבל מהלקוח ומעדכנת את רשימת הנוכחים על המסך 
     def handling_server_msg(self,string):
-        
+        self.anotherone=[]
         images=[[]]
         for i in range (3):
             images.append([])
@@ -118,49 +124,8 @@ class client:
                         self.anotherone.append(tapir)
         
        
-        
-        # num=0
-        # banana=[[]]
-        
-        # for a in range(len(self.all_image)):  
-            # for n in range(len(self.all_image[a])):
-                # banana[a].append(self.all_image[a][n])
-            # banana.append([])
-            
-        
-        # for y in range (len(banana)):
-            # for m in range (len(banana[y])):
-                # for choice in self.anotherone:
-                    # if (y,m)==choice[0]:
-                        # (s,h)=choice[1]
-                        
-                        # banana[y][m]=banana[s][h]
-                        # banana[s][h]=self.all_image[y][m]
-        
-        # img=[[]]
-        # for o in range (3):
-            # for c in range (3):
-                
-                # img[o].append(ImageTk.PhotoImage(banana[o][c]))   
-            # img.append([])
-                
-        # for g in range(3):
-            # for b in range (3):
-                    
-                    # panel = tk.Button(self.root, image = img[g][b])
-                    
-                    # panel.grid(row=b,column=g)
-        # for label in self.root.grid_slaves():
-                    # flagy=False
-                    # (s,h)= (int(label.grid_info()["row"]),int(label.grid_info()["column"]))
-                    
-                    # for choice1 in self.anotherone:
-                        # if (s,h)==choice1[0]:
-                            # flagy=True
-                    # if flagy==False:
-                        # label.config(image="" ,background="black")
-        # self.root.after(100,self.create_panel)
-        # self.root.mainloop()
+       
+    # הפעולה היוצרת את הלוח הראשוני ומוסיפה אליו את התמונות ואת הכפתורים יוצרת רשימה של כפתורים ואת מסך המחסן 
     def create_panel(self):
         img=[[]]
         mix=self.items
@@ -207,6 +172,7 @@ class client:
                       
         self.root.after(10,self.game)
         self.root.mainloop()    
+    #פעולה הבודקת האם המשתתפים סיימו את הפאזל 
     def check(self,string):
         tuple_Array=string.split("/r/n")
         
@@ -224,7 +190,7 @@ class client:
             return False
         return True
 
-        
+    #פעולה היוצרת מערך של תמונות חתוכות אשר משלימות את התמונה 
     def crop_Array(self,imageObject):
         x1=0
         y1=0
@@ -238,9 +204,12 @@ class client:
             x1=x1+(imageObject.size[0]/3)
             all_image.append([])
         return all_image
+    #פעולה המחזירה את רשימת ה items
     def show_items(self):
 
         return self.items
+    #הפעולה שבה בעצם קורה המשחק מחכה ללחיצת השחקן על התמונה ואז משנה את המסך למסך לוח הפאזל ומחכה לכך שיבחר מיקום על המסך ואחרי הבחירה שלו מחזירה למסך המחסן 
+    #ושולחת את המידע על התזוזה לשרת 
     def game(self):
         socket=self.server_socket
         flag=False
@@ -306,7 +275,8 @@ class client:
                         ##label.config(image="",background="black")
                         if (kkk==(i,t)):
                             str1=(str(self.items[i][t][1][0])+"/r/n"+str(self.items[i][t][1][1])+"/r/n"+str(x1)+"/r/n"+str(y1))
-                            socket.send(str1.encode())
+                socket.send(str1.encode())
+                print ("sennttttttttttttttttttttttttttttttt")
                     
                 if flag==False:
                     print ("your numbers were wrong")
@@ -315,14 +285,14 @@ class client:
         
         
         
-        while True:
-            
-            data=socket.recv(1024)
-            if data.decode()!='' and data.decode()!=str1:
-                print (data)
-                self.handling_server_msg(data.decode())
-                print ("outttttttttttttttttttttttttt")
-                break
+        #while True:
+        print ("reciving")
+        data=socket.recv(1024)
+        if data.decode()!='':
+            print (data)
+            self.handling_server_msg(data.decode())
+            print ("outttttttttttttttttttttttttt")
+            #break
 
         self.lastdata=self.lastdata+data.decode()
         
@@ -369,7 +339,7 @@ while(sent1==False):
        
       
         data = my_socket.recv(1024)
-
+        #תהליך קבלת התמונה מהשרת 
         print (data.decode())
         if (data.decode()=="sendmesendme"):
             
--- serv.py
+++ serv.py
@@ -16,19 +16,19 @@ messages_to_send=[]
 
 class game:
     def __init__(self,ImageObject,open_client_sockets):
-        self.ImageObject=ImageObject
-        self.open_client_sockets=open_client_sockets
-        mix=self.mixing_numbers()
-        self.items=[[]]
-        self.empty=[]
+        self.ImageObject=ImageObject# תמונת פתירת הפאזל
+        self.open_client_sockets=open_client_sockets# רשימת השחקנים המשתתפים
+        mix=self.mixing_numbers()#המיקום של כל חתיכה בפאזל כפי שהיא נשלחת ללקוח
+        self.items=[[]]#רשימת חלקי הפאזל המעורביםכפי שהם קיבלו ברנדומליות במיקס 
+        self.empty=[]# רשימת החלקים המתקבלת כל  פעם מהלקוחות
         
-        
+        #מכניסים את הערכים של מיקס לתוך אייטמס
         for i in range (len(mix)):
             for t in range (len(mix[i])):
                 self.items[i].append(mix[i][t])
             self.items.append([])
                 
-        
+    #פעולת ההתחלה בה השרת שולח לכל אחד מהלקוחות מה הם החלקים שלהם     
     def start1(self):
         
         wlist=self.open_client_sockets
@@ -46,32 +46,42 @@ class game:
                     if i!=(len(mix)-1):   
                         sender=sender+"|||"
                 wlist[g].send(sender.encode())
-
+    #תפעולת המשחק עצמו השרת יקבל כל תור את המידע מאחד מהלקוחות וימיר אותו לשורת טקסט בצורה שבה הלקוח יבין וישלח את המידע לשני הלקוחות 
     def play(self):
-        
-        for socket in self.open_client_sockets:
+    
+        global server_socket
+        rlist,wlist,xlist=select.select([server_socket]+self.open_client_sockets,self.open_client_sockets,[])
+        messages_to_send=[]
+        chachaw=False
+        for socket in rlist:
             
-            while (True):
-                string=socket.recv(1024).decode()
-                if (string!=''):
-                    print ("recveddddd")
-                    arr=string.split("/r/n")
-                    x=arr[0]
-                    y=arr[1]
-                    x1=arr[2]
-                    y1=arr[3]
-                    tupl=(x,y,x1,y1)
+            #while (True):
+            string=socket.recv(1024).decode()
+            if (string!=''):
+                print ("recveddddd")
+                arr=string.split("/r/n")
+                x=arr[0]
+                y=arr[1]
+                x1=arr[2]
+                y1=arr[3]
+                tupl=(x,y,x1,y1)
+                for ttt in self.empty:
+                    if x1 in ttt and y1 in ttt:
+                        chachaw=True
+                if chachaw==False:
                     self.empty.append(tupl)
-                    break
+                #break
             string1=""
             for tup in self.empty:
                 
                 
                 string1=string1+"/r/n"+tup[2]+","+tup[3]+"|"+tup[1]+","+tup[0]
-                
-            for my_socket in open_client_sockets:
-                if my_socket!=socket:
-                    my_socket.send(string1.encode())
+            messages_to_send.append((socket,string1))   
+            # for my_socket in open_client_sockets:
+                # if my_socket!=socket:
+                    # my_socket.send(string1.encode())
+            self.send_waiting_messages1(self.open_client_sockets,messages_to_send)
+    # פעולת ההכנה לשליחה של חלקי הפאזל ללקוח שולחת לשחקן הראשון חצי ולשחקן השני חצי מהחלקים המעורבבים במיקס                 
     def to_send(self,name):
         if (name==0):
             
@@ -97,7 +107,7 @@ class game:
                 mix1[1].append(popai)
             
             return mix1 
-# send messages that weren't sent
+    #פעולה היוצרת עירבוב של מיקומים במערך דו מימדי 
     def mixing_numbers(all_image):
         list1=[[]]
         list1.append([])
@@ -137,7 +147,21 @@ class game:
                    
                 Flag=False		
                 
-        return mix             
+        return mix  
+        
+        
+    #פעולה המקבלת רשימה של הודעות  ושולחת אותן ללקוחות השונים        
+    def send_waiting_messages1(self,wlist, messages_to_send):
+        for message in messages_to_send:
+            (client_socket, data) = message
+            
+            # if it possible to write to the socket
+            for my_socket in wlist:
+                if my_socket!=client_socket:
+                    client_socket.send(data.encode())
+                    print ("sent")
+            messages_to_send.remove(message)
+#פעולה המקבלת רשימה של הודעות  ושולחת אותן ללקוחות השונים        
 def send_waiting_messages(wlist, messages_to_send):
     for message in messages_to_send:
         (client_socket, data) = message
@@ -153,61 +177,64 @@ while (not sent):
     rlist,wlist,xlist=select.select([server_socket]+open_client_sockets,open_client_sockets,[])
     
     for current_socket in rlist:
+        # אם לקוח הוא לקוח חדש יצטרף לתוך רדשימת הלקוחות הפתוחים 
         if current_socket is server_socket:
             (new_socket,adress)=server_socket.accept()
             if new_socket not in open_client_sockets:
                 open_client_sockets.append(new_socket)
            
         else:
+            
+            # read the data from the client
+            data = current_socket.recv(1024)
+            
+            #אם אחד הלקוחותישלח את מילת הקוד אז השרת ישלח לו אותה חזרה 
+            if (data.decode()=="sendme" and len(open_client_sockets)==2):
+                for ha_socket in open_client_sockets:
+                        ha_socket.send("sendme".encode())
+            #אחרי שהלקוחקיבלאת מילת הקוד הוא ישלח שהוא מוכן לקבל את התמונה ואז השרת ישלח לו את תמונת הפאזל             
+            if data.decode()=="ready":
+                preperd.append(current_socket)
+                waiting=True
                 
-                # read the data from the client
-                data = current_socket.recv(1024)
-                
-                
-                if (data.decode()=="sendme" and len(open_client_sockets)==2):
-                    for ha_socket in open_client_sockets:
-                            ha_socket.send("sendme".encode())
-                            
-                if data.decode()=="ready":
-                    preperd.append(current_socket)
-                    waiting=True
-                    
-                if(len(preperd)==len(open_client_sockets)):
-                    with open("comp.jpg", 'rb') as filesent:
-                        data = filesent.read()
-                        file_len = len(data)
+            if(len(preperd)==len(open_client_sockets)):
+                with open("comp.jpg", 'rb') as filesent:
+                    data = filesent.read()
+                    file_len = len(data)
 
-                        msg_data1 = file_len
-                        msg = str(msg_data1)
-                        
-                        msg = msg.encode('latin-1')
-                        for socket in open_client_sockets:
-                            socket.send(msg)
-                        #data = data.encode('latin-1')
-                        for socket in open_client_sockets:
-                            if socket in wlist:
-                                socket.sendall(data)
-                                
-                         #wait here untill finish to send the whole fil
-                        sent=True
-                        
-                
-                
-                
-                
-                
-                
-                
-                elif(waiting==False):
-                    for the_socket in open_client_sockets:
+                    msg_data1 = file_len
+                    msg = str(msg_data1)
                     
-                        # add the message from the client to the list of the messages that weren't sent
-                        messages_to_send.append((the_socket, data.decode()))
-          
+                    msg = msg.encode('latin-1')
+                    for socket in open_client_sockets:
+                        socket.send(msg)
+                    #data = data.encode('latin-1')
+                    for socket in open_client_sockets:
+                        if socket in wlist:
+                            socket.sendall(data)
+                            
+                     #wait here untill finish to send the whole fil
+                    sent=True#מסמן שהשליחהקרתה ומפסיק את הלולאה ויתחיל את המשחק
+                    
+            
+            
+            
+            
+            
+            
+            
+            elif(waiting==False):
+                for the_socket in open_client_sockets:
+                
+                    # add the message from the client to the list of the messages that weren't sent
+                    messages_to_send.append((the_socket, data.decode()))
+      
     send_waiting_messages(wlist,messages_to_send)
 imageObject=Image.open("comp.jpg")
 
 g1=game(imageObject,open_client_sockets)
+#שליחה ללקוחות את רשימת החלקים שלהם 
 g1.start1()
 while True:
+#תחילת המשחק 
     g1.play()
